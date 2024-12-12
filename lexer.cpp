f666bbb5864980092647bd294c3e8a85a8bd1b60
Aliqyan-21 <aliqyanabid21@gmail.com>
2024-12-13 02:31:34 +0530

    feat: more tests, lexer error handling, right and left parenthesis in token types

--- lexer.cpp
+++ lexer.cpp
@@ -6,7 +6,7 @@ Lexer::Lexer(const std::string &code) : code(code) {}
 // tokenize function returns the tokens vector
 std::vector<Token> Lexer::tokenize() {
   std::vector<Token> tokens;
-  while (pos <= code.size()) {
+  while (pos < code.size()) {
     char c = code[pos];
 
     // whitespace
@@ -18,11 +18,9 @@ std::vector<Token> Lexer::tokenize() {
     // alphabet/identifier/keyword
     else if (isalpha(c)) {
       std::string identifier;
-      while (isalpha(code[pos])) {
-        identifier += code[pos];
-        pos++;
+      while (pos < code.size() && isalpha(code[pos])) {
+        identifier += code[pos++];
       }
-      pos--;
       if (identifier == "if") {
         tokens.push_back({tokenType::TOKEN_IF, identifier});
       } else if (identifier == "int") {
@@ -35,35 +33,59 @@ std::vector<Token> Lexer::tokenize() {
     // number
     else if (isdigit(c)) {
       std::string num;
-      while (isdigit(code[pos])) {
-        num += code[pos];
-        pos++;
+      while (pos < code.size() && isdigit(code[pos])) {
+        num += code[pos++];
       }
-      pos--;
       tokens.push_back({tokenType::TOKEN_NUMBER, num});
     }
 
     // special chars like '+', '-', '=', '==', '(', ')', '{', '}', ';'
-    else if (c == '=' && pos <= code.size() && code[pos + 1] == '=') {
-      tokens.push_back({tokenType::TOKEN_EQUAL, "=="});
-    } else if (c == '=') {
-      tokens.push_back({tokenType::TOKEN_ASSIGN, "="});
-    } else if (c == '+') {
-      tokens.push_back({tokenType::TOKEN_PLUS, "+"});
-    } else if (c == '-') {
-      tokens.push_back({tokenType::TOKEN_MINUS, "-"});
-    } else if (c == '(') {
-      tokens.push_back({tokenType::TOKEN_LBRACE, "("});
-    } else if (c == ')') {
-      tokens.push_back({tokenType::TOKEN_RBRACE, ")"});
-    } else if (c == '{') {
-      tokens.push_back({tokenType::TOKEN_LBRACE, "{"});
-    } else if (c == '}') {
-      tokens.push_back({tokenType::TOKEN_RBRACE, "}"});
-    } else if (c == ';') {
-      tokens.push_back({tokenType::TOKEN_SEMICOLON, ";"});
+    else {
+      switch (c) {
+      case '=':
+        if (pos + 1 < code.size() && code[pos + 1] == '=') {
+          tokens.push_back({tokenType::TOKEN_EQUAL, "=="});
+          pos += 2;
+        } else {
+          tokens.push_back({tokenType::TOKEN_ASSIGN, "="});
+          pos++;
+        }
+        break;
+      case '+':
+        tokens.push_back({tokenType::TOKEN_PLUS, "+"});
+        pos++;
+        break;
+      case '-':
+        tokens.push_back({tokenType::TOKEN_MINUS, "-"});
+        pos++;
+        break;
+      case '(':
+        tokens.push_back({tokenType::TOKEN_LBRACE, "("});
+        pos++;
+        break;
+      case ')':
+        tokens.push_back({tokenType::TOKEN_RBRACE, ")"});
+        pos++;
+        break;
+      case '{':
+        tokens.push_back({tokenType::TOKEN_LPAREN, "{"});
+        pos++;
+        break;
+      case '}':
+        tokens.push_back({tokenType::TOKEN_RPAREN, "}"});
+        pos++;
+        break;
+      case ';':
+        tokens.push_back({tokenType::TOKEN_SEMICOLON, ";"});
+        pos++;
+        break;
+      default:
+        tokens.push_back({tokenType::TOKEN_UNKNOWN, ""});
+        pos++;
+        std::cerr << "Invalid token : " << c << std::endl;
+        break;
+      }
     }
-    pos++;
   }
 
   tokens.push_back({tokenType::TOKEN_EOF});
--- tests/test_lexer.cpp
+++ tests/test_lexer.cpp
@@ -1,13 +1,37 @@
 #include "../lexer.h"
 
 int main(void) {
-  std::string code = "int a; int b; int c; a = 10; b = 20; c = a + b; if (c == "
-                     "30) { c = c + 1; }";
+  // empty string
+  std::string code1 = "";
 
-  Lexer lex(code);
-  std::vector<Token> tokens = lex.tokenize();
+  // invalid token
+  std::string code2 = "int i; i = @";
 
-  for (auto &t : tokens) {
+  // valid code
+  std::string code3 =
+      "int a; int b; int c; a = 10; b = 20; c = a + b; if (c == "
+      "30) { c = c + 1; }";
+
+  std::cout << "tokens 1: " << std::endl;
+  Lexer lex1(code1);
+  std::vector<Token> tokens1 = lex1.tokenize();
+  for (auto &t : tokens1) {
+    std::cout << t.name << " : " << t.value << std::endl;
+  }
+
+  std::cout << std::endl;
+  std::cout << "tokens 2: " << std::endl;
+  Lexer lex2(code2);
+  std::vector<Token> tokens2 = lex2.tokenize();
+  for (auto &t : tokens2) {
+    std::cout << t.name << " : " << t.value << std::endl;
+  }
+
+  std::cout << std::endl;
+  Lexer lex3(code3);
+  std::vector<Token> tokens3 = lex3.tokenize();
+  std::cout << "tokens 3: " << std::endl;
+  for (auto &t : tokens3) {
     std::cout << t.name << " : " << t.value << std::endl;
   }
 
